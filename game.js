0dae4b05604105741a1a3f97a1d2981df79e5791
berry <kathborowy@gmail.com>
2022-05-30 13:27:21 +0200

    created rps function, there usermove and computermove are case insensitive

--- game.js
+++ game.js
@@ -2,3 +2,8 @@ function computer() {
     let move = Math.floor(Math.random() * 3) + 1;
     return (move == 3) ? "Rock" : (move == 2) ? "Paper" : "Scissors"
 }
+
+function RPS() {
+    let userMove = prompt("?").toUpperCase();
+    let computerMove = computer().toUpperCase();
+}
