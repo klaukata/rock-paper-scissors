150570105afee0baf29553ac07d298d211d7ad3b
berry <kathborowy@gmail.com>
2022-05-30 13:50:24 +0200

    changed if statements to upper case because of comparison bug (ROCK != rock)

--- game.js
+++ game.js
@@ -6,31 +6,29 @@ function computer() {
 function RPS() {
     let userMove = prompt("?").toUpperCase();
     let computerMove = computer().toUpperCase();
-    if (userMove == "Rock") {
-        if (computerMove == "Rock") {
-            console.log("It's a tie!");
-          } else if (computerMove == "Paper") {
-            console.log("You lose! Paper beats Rock")
-          } else {
-            console.log("You win! Rock beats Scissors")
+    if (userMove == "ROCK") {
+        if (computerMove == "ROCK") {
+          console.log("It's a tie!");
+        } else if (computerMove == "PAPER") {
+          console.log("You lose! Paper beats Rock")
+        } else {
+          console.log("You win! Rock beats Scissors")
         }
-      } 
-      else if (userMove == "Paper") {
-        if (computerMove == "Rock") {
-            console.log("You win! Paper beats Rock")
-          } else if (computerMove == "Paper") {
-            console.log("It's a tie!");
-          } else {
-            console.log("You lose! Scissors beat Paper")
+      } else if (userMove == "PAPER") {
+        if (computerMove == "ROCK") {
+          console.log("You win! Paper beats Rock")
+        } else if (computerMove == "PAPER") {
+          console.log("It's a tie!");
+        } else {
+          console.log("You lose! Scissors beat Paper")
         }
-      } 
-      else {
-        if (computerMove == "Rock") {
-            console.log("You lose! Rock beats Scissors")
-          } else if (computerMove == "Paper") {
-            console.log("You win! Scissors beat Paper")
-          } else {
-            console.log("It's a tie!");
+      } else {
+        if (computerMove == "ROCK") {
+          console.log("You lose! Rock beats Scissors")
+        } else if (computerMove == "PAPER") {
+          console.log("You win! Scissors beat Paper")
+        } else {
+          console.log("It's a tie!333");
         }
-      }
+    }
 }
