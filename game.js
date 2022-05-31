3c640d5483ce07001a7b85eb4e5968a8aeef759a
berry <kathborowy@gmail.com>
2022-05-31 11:11:21 +0200

    game() without console.log

--- game.js
+++ game.js
@@ -46,8 +46,6 @@ function game() {
     } else if (result.includes("You lose!")) {
       computerScore++
     }
-    console.log(result)
-    console.log(userScore + "       " + computerScore)
   }
   let userToComp =  userScore + " to " + computerScore
   return (userScore > computerScore) ? 'You won! ' + userToComp : (userScore < computerScore) ? 'You lost this one :/ ' + userToComp : "It's a tie"
