b6d70f5ea8235138ca481a7b671278dbe371b868
berry <kathborowy@gmail.com>
2022-05-31 06:28:43 +0200

    fixed hoisitng issue

--- game.js
+++ game.js
@@ -1,34 +1,34 @@
 function computer() {
-    let move = Math.floor(Math.random() * 3) + 1;
-    return (move == 3) ? "Rock" : (move == 2) ? "Paper" : "Scissors"
+  let move = Math.floor(Math.random() * 3) + 1;
+  return (move == 3) ? "Rock" : (move == 2) ? "Paper" : "Scissors"
 }
+let userMove = "PAPER" //prompt("?").toUpperCase();
+let computerMove = computer().toUpperCase();
 
-function RPS() {
-    let userMove = prompt("?").toUpperCase();
-    let computerMove = computer().toUpperCase();
-    if (userMove == "ROCK") {
-        if (computerMove == "ROCK") {
-          console.log("It's a tie!");
-        } else if (computerMove == "PAPER") {
-          console.log("You lose! Paper beats Rock")
-        } else {
-          console.log("You win! Rock beats Scissors")
-        }
-      } else if (userMove == "PAPER") {
-        if (computerMove == "ROCK") {
-          console.log("You win! Paper beats Rock")
-        } else if (computerMove == "PAPER") {
-          console.log("It's a tie!");
-        } else {
-          console.log("You lose! Scissors beat Paper")
-        }
+function RPS(usr, comp) {
+  if (usr == "ROCK") {
+      if (comp == "ROCK") {
+        console.log("It's a tie!");
+      } else if (comp == "PAPER") {
+        console.log("You lose! Paper beats Rock")
       } else {
-        if (computerMove == "ROCK") {
-          console.log("You lose! Rock beats Scissors")
-        } else if (computerMove == "PAPER") {
-          console.log("You win! Scissors beat Paper")
-        } else {
-          console.log("It's a tie!333");
-        }
-    }
+        console.log("You win! Rock beats Scissors")
+      }
+    } else if (usr == "PAPER") {
+      if (comp == "ROCK") {
+        console.log("You win! Paper beats Rock")
+      } else if (comp == "PAPER") {
+        console.log("It's a tie!");
+      } else {
+        console.log("You lose! Scissors beat Paper")
+      }
+    } else {
+      if (comp == "ROCK") {
+        console.log("You lose! Rock beats Scissors")
+      } else if (comp == "PAPER") {
+        console.log("You win! Scissors beat Paper")
+      } else {
+        console.log("It's a tie!333");
+      }
+  }
 }
