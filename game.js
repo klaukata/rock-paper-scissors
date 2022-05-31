bcfc6e37f3652dbae7bda5a0f9573d310ece5ced
berry <kathborowy@gmail.com>
2022-05-31 11:20:18 +0200

    welcome message

--- game.js
+++ game.js
@@ -1,3 +1,5 @@
+console.log("------ROCK-PAPER-SCISSORS------")
+
 function computer() {
   let move = Math.floor(Math.random() * 3) + 1;
   return (move == 3) ? "Rock" : (move == 2) ? "Paper" : "Scissors"
